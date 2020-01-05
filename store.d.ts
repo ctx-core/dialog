d8af724a3b06230c432e5b586911a02dfca7ade4
Brian Takita <brian.takita@gmail.com>
2020-01-04 19:03:02 -0500

    feat: store: b__ factories
    ∋ "@ctx-core/object": "^6.5.0"

--- store.d.ts
+++ store.d.ts
@@ -1,6 +1,11 @@
-export declare const __stack__dialog: import("svelte/store").Writable<any[]>;
-export declare const __top__stack__dialog: import("svelte/store").Readable<any>;
-export declare function add__component__stack__dialog(component: any): void;
-export declare function remove__component__stack__dialog(component: any): void;
-export declare function close__top__stack__dialog(event: any): void;
-export declare function close__stack__dialog(event?: any): void;
+import { Writable } from 'svelte/store';
+export interface Writable__stack__dialog extends Writable<any[]> {
+    add__component__stack__dialog: (component: any) => void;
+    remove__component__stack__dialog: (component: any) => void;
+    close__top__stack__dialog: (event: Event) => void;
+    close__stack__dialog: (event?: Event) => void;
+}
+export declare const b__stack__dialog: (ctx?: any, opts?: any) => Writable__stack__dialog;
+export declare const __stack__dialog: Writable__stack__dialog;
+export declare const add__component__stack__dialog: (component: any) => void, remove__component__stack__dialog: (component: any) => void, close__top__stack__dialog: (event: Event) => void, close__stack__dialog: (event?: Event) => void;
+export declare const b__top__stack__dialog: (ctx?: any, opts?: any) => import("svelte/store").Readable<any>;
--- store.ts
+++ store.ts
@@ -1,24 +1,47 @@
-import { writable, derived, get } from 'svelte/store'
+import { writable, derived, get, Writable } from 'svelte/store'
 // @ts-ignore
 import { subscribe__debug } from '@ctx-core/store'
 import { each, _last, remove } from '@ctx-core/array'
-export const __stack__dialog = writable([])
-export const __top__stack__dialog = derived(__stack__dialog, _last)
-export function add__component__stack__dialog(component) {
-	const stack__dialog = get(__stack__dialog).slice(0)
-	stack__dialog.push(component)
-	__stack__dialog.set(stack__dialog)
+import { assign, _b } from '@ctx-core/object'
+export interface Writable__stack__dialog extends Writable<any[]> {
+	add__component__stack__dialog:(component:any)=>void
+	remove__component__stack__dialog:(component:any)=>void
+	close__top__stack__dialog:(event:Event)=>void
+	close__stack__dialog:(event?:Event)=>void
 }
-export function remove__component__stack__dialog(component) {
-	const stack__dialog = get(__stack__dialog).slice(0)
-	remove(stack__dialog, component)
-	__stack__dialog.set(stack__dialog)
-}
-export function close__top__stack__dialog(event) {
-	const top__stack__dialog = get(__top__stack__dialog)
-	if (top__stack__dialog) top__stack__dialog.close(event)
-}
-export function close__stack__dialog(event?) {
-	const stack__dialog = get(__stack__dialog)
-	each(stack__dialog, dialog => dialog.close(event))
-}
+export const b__stack__dialog = _b<Writable__stack__dialog>('__stack__dialog', ctx=>{
+	const __stack__dialog = writable([])
+	return assign(__stack__dialog, {
+		add__component__stack__dialog,
+		remove__component__stack__dialog,
+		close__top__stack__dialog,
+		close__stack__dialog,
+	})
+	function add__component__stack__dialog(component) {
+		const stack__dialog = get(__stack__dialog).slice(0)
+		stack__dialog.push(component)
+		__stack__dialog.set(stack__dialog)
+	}
+	function remove__component__stack__dialog(component) {
+		const stack__dialog = get(__stack__dialog).slice(0)
+		remove(stack__dialog, component)
+		__stack__dialog.set(stack__dialog)
+	}
+	function close__top__stack__dialog(event) {
+		const top__stack__dialog = get(b__top__stack__dialog(ctx))
+		if (top__stack__dialog) top__stack__dialog.close(event)
+	}
+	function close__stack__dialog(event?) {
+		const stack__dialog = get(__stack__dialog)
+		each(stack__dialog, dialog=>dialog.close(event))
+	}
+})
+export const __stack__dialog = b__stack__dialog()
+export const {
+	add__component__stack__dialog,
+	remove__component__stack__dialog,
+	close__top__stack__dialog,
+	close__stack__dialog,
+} = __stack__dialog
+export const b__top__stack__dialog = _b('__top__stack__dialog', ctx=>
+	derived(b__stack__dialog(ctx), _last))
