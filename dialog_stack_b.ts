244f65728630c20fac538a8aa9e5a1bcb7946350
Brian Takita <brian.takita@gmail.com>
2020-12-16 23:01:20 -0700

    dialog_stack_b: inferred type

--- dialog_stack_b.ts
+++ dialog_stack_b.ts
@@ -2,13 +2,13 @@ import { _b, assign } from '@ctx-core/object'
 import { get, Writable, writable } from '@ctx-core/store'
 import { each, remove } from '@ctx-core/array'
 import type { dialog_type } from './dialog_stack_top_b'
-export const dialog_stack_b = _b<dialog_stack_type>('dialog_stack', ()=>{
+export const dialog_stack_b = _b('dialog_stack', ()=>{
 	const dialog_stack = writable([] as $dialog_stack_type) as dialog_stack_type
 	return assign(dialog_stack, {
 		add_dialog_stack_component,
 		remove_dialog_stack_component,
 		close_dialog_stack,
-	})
+	}) as dialog_stack_type
 	function add_dialog_stack_component(component) {
 		const $dialog_stack = get(dialog_stack).slice(0) as $dialog_stack_type
 		$dialog_stack.push(component)
