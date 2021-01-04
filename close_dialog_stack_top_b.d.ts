6eb2d014f454322f1e0b3e2e7993575d7393a035
Brian Takita <brian.takita@gmail.com>
2021-01-03 20:21:01 -0500

    update dependencies

--- close_dialog_stack_top_b.d.ts
+++ close_dialog_stack_top_b.d.ts
@@ -1,1 +1,1 @@
-export declare const close_dialog_stack_top_b: (ctx: object, opts?: import("@ctx-core/object")._be_opts_type | undefined) => (event: any) => void;
+export declare const close_dialog_stack_top_b: import("@ctx-core/object").be_type<(event: any) => void, object>;
--- dialog_stack_b.d.ts
+++ dialog_stack_b.d.ts
@@ -1,6 +1,6 @@
 import { Writable } from '@ctx-core/store';
 import type { dialog_type } from './dialog_stack_top_b';
-export declare const dialog_stack_b: (ctx: object, opts?: import("@ctx-core/object")._be_opts_type | undefined) => dialog_stack_type;
+export declare const dialog_stack_b: import("@ctx-core/object").be_type<dialog_stack_type, object>;
 export declare type $dialog_stack_type = dialog_type[];
 export interface dialog_stack_type extends Writable<$dialog_stack_type> {
     add_dialog_stack_component: (component: unknown) => void;
